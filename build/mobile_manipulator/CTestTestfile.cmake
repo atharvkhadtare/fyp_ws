5a01029acec5d96e8473b6f347b59c17f871123f
atharvkhadtare <atharvkhadtare@gmail.com>
2019-01-02 14:13:35 +0530

    Deleting build and devel

--- build/mobile_manipulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rshah/ROS/fyp_ws/src/mobile_manipulator
-# Build directory: /home/rshah/ROS/fyp_ws/build/mobile_manipulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
