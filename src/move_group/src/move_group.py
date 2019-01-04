c42eba711188976fa1973c942ea6d616e9f021c5
atharvkhadtare <atharvkhadtare@gmail.com>
2019-01-04 19:44:37 +0530

    single planning group

--- src/move_group/src/move_group.py
+++ src/move_group/src/move_group.py
@@ -18,7 +18,7 @@ robot = moveit_commander.RobotCommander()
 
 scene = moveit_commander.PlanningSceneInterface()
 
-group_name = "arm"
+group_name = "panda_arm"
 group = moveit_commander.MoveGroupCommander(group_name)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
@@ -69,14 +69,14 @@ print ""
 # print robot.get_current_state()
 # print ""
 
-group_hand = moveit_commander.MoveGroupCommander("hand")
-print "============ Printing hand pose"
-print group_hand.get_current_pose()
-print ""
+# group_hand = moveit_commander.MoveGroupCommander("hand")
+# print "============ Printing hand pose"
+# print group_hand.get_current_pose()
+# print ""
 
 pose_goal = geometry_msgs.msg.Pose()
 pose_goal.orientation.w = 1.0
-pose_goal.position.x = 0
+pose_goal.position.x = 0.1
 pose_goal.position.y = 0.129
 pose_goal.position.z = 0.192
 group.set_pose_target(pose_goal)
@@ -92,10 +92,10 @@ print "============ Printing robot state"
 print robot.get_current_state()
 print ""
 
-group_hand = moveit_commander.MoveGroupCommander("hand")
-print "============ Printing hand pose"
-print group_hand.get_current_pose()
-print ""
+# group_hand = moveit_commander.MoveGroupCommander("hand")
+# print "============ Printing hand pose"
+# print group_hand.get_current_pose()
+# print ""
 # # It is always good to clear your targets after planning with poses.
 # # Note: there is no equivalent function for clear_joint_value_targets()
 group.clear_pose_targets()
