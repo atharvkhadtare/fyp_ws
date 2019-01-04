ce5d126edc1ee635a7d15c201fd4aa6c55e6c230
rshah4356 <rshah4356@gmail.com>
2019-01-04 23:53:18 +0530

    Kinematic Solver changed in config file

--- src/move_group/src/move_group.py
+++ src/move_group/src/move_group.py
@@ -18,7 +18,7 @@ robot = moveit_commander.RobotCommander()
 
 scene = moveit_commander.PlanningSceneInterface()
 
-group_name = "panda_arm"
+group_name = "arm"
 group = moveit_commander.MoveGroupCommander(group_name)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
@@ -42,60 +42,59 @@ print "============ Robot Groups:", robot.get_group_names()
 print "============ Printing group_arm pose"
 print group.get_current_pose()
 print ""
-# print "============ Printing robot state"
-# print robot.get_current_state()
-# print "====join_ngoal"
+
 
 # We can get the joint values from the group and adjust some of the values:
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = 0
-joint_goal[1] = 0.0785
-joint_goal[2] = 0
-# joint_goal[3] = 0
-# joint_goal[4] = 0
+# joint_goal = group.get_current_joint_values()
+# joint_goal[0] = 0
+# joint_goal[1] = 0.785
+# joint_goal[2] = 0
+# joint_goal[3] = -0.785
+# # joint_goal[4] = 0.78
 
-# The go command can be called with joint values, poses, or without any
-# parameters if you have already set the pose or joint target for the group
-group.go(joint_goal, wait=False)
+# # The go command can be called with joint values, poses, or without any
+# # parameters if you have already set the pose or joint target for the group
+# group.go(joint_goal, wait=False)
 
-# Calling ``stop()`` ensures that there is no residual movement
-group.stop()
+# # Calling ``stop()`` ensures that there is no residual movement
+# group.stop()
 
-
-print "============ Printing group_arm pose"
-print group.get_current_pose()
-print ""
+# print "============ Printing group_arm pose"
+# print group.get_current_pose()
+# print ""
 # print "============ Printing robot state"
 # print robot.get_current_state()
-# print ""
-
-# group_hand = moveit_commander.MoveGroupCommander("hand")
-# print "============ Printing hand pose"
-# print group_hand.get_current_pose()
 # print ""
 
 pose_goal = geometry_msgs.msg.Pose()
 pose_goal.orientation.w = 1.0
-pose_goal.position.x = 0.1
-pose_goal.position.y = 0.129
-pose_goal.position.z = 0.192
+pose_goal.position.x = 0
+pose_goal.position.y = 0#-0.2668
+pose_goal.position.z = 0.441 #0.3305
 group.set_pose_target(pose_goal)
 # pose_goal = group.get_random_pose()
 plan = group.go(wait=True)
-# # Calling `stop()` ensures that there is no residual movement
+# # # Calling `stop()` ensures that there is no residual movement
 group.stop()
-
+print "Goal Reached"
 print "============ Printing group_arm pose"
 print group.get_current_pose()
 print ""
-print "============ Printing robot state"
-print robot.get_current_state()
-print ""
 
-# group_hand = moveit_commander.MoveGroupCommander("hand")
-# print "============ Printing hand pose"
-# print group_hand.get_current_pose()
+print "============ Printing group_arm pose"
+print group.get_current_joint_values()
+print ""
+# print "============ Printing group_arm pose"
+# print group.get_current_pose()
 # print ""
-# # It is always good to clear your targets after planning with poses.
-# # Note: there is no equivalent function for clear_joint_value_targets()
+# print "============ Printing robot state"
+# print robot.get_current_state()
+# print ""
+
+# # group_hand = moveit_commander.MoveGroupCommander("hand")
+# # print "============ Printing hand pose"
+# # print group_hand.get_current_pose()
+# # print ""
+# # # It is always good to clear your targets after planning with poses.
+# # # Note: there is no equivalent function for clear_joint_value_targets()
 group.clear_pose_targets()
