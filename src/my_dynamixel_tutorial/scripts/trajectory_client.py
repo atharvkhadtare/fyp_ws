8c282f5e7e5e8ab1756b614386b41c863fce424b
majorviraj <majorviraj@gmail.com>
2019-01-09 18:27:34 +0530

    trash commit for atharv

--- src/my_dynamixel_tutorial/scripts/trajectory_client.py
+++ src/my_dynamixel_tutorial/scripts/trajectory_client.py
@@ -10,8 +10,6 @@ import control_msgs.msg
 from trajectory_msgs.msg import JointTrajectoryPoint
 from control_msgs.msg import JointTrajectoryAction, JointTrajectoryGoal, FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 
-
-
 class Joint:
         def __init__(self, motor_name):
             #arm_name should be b_arm or f_arm
@@ -20,7 +18,6 @@ class Joint:
             rospy.loginfo('Waiting for joint trajectory action')
             self.jta.wait_for_server()
             rospy.loginfo('Found joint trajectory action!')
-
 
         def move_joint(self, angles):
             goal = FollowJointTrajectoryGoal()
@@ -33,12 +30,10 @@ class Joint:
             goal.trajectory.points.append(point)
             self.jta.send_goal_and_wait(goal)
 
-
 def main():
-            arm = Joint('f_arm')
+            arm = Joint('arm')
             arm.move_joint([-3.14])
             # arm.move_joint([6.28,3.14,6.28])
-
 
 if __name__ == '__main__':
       rospy.init_node('joint_position_tester')
