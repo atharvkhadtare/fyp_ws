7d0af8ad0c3326d7e0890596592283931e56b902
majorviraj <majorviraj@gmail.com>
2019-01-10 15:53:57 +0530

    *joint_state_publisher and state_publishers(TF) working *Telemetry visualization working. wrist_pitch motor axis to be changed in URDF.
    *Change static_transform_publisher arguements in robot_state.launch accrding to virtual joint.
    
    	new file:   src/mobile_manipulator_moveit_config/config/controllers.yaml
    	modified:   src/mobile_manipulator_moveit_config/launch/mobile_manipulator_moveit_controller_manager.launch.xml
    	new file:   src/mobile_manipulator_moveit_config/launch/moveit_planning_execution.launch
    	new file:   src/my_dynamixel_tutorial/arm.rviz
    	new file:   src/my_dynamixel_tutorial/launch/robot_state.launch
    	new file:   src/my_dynamixel_tutorial/launch/rviz.launch
    	modified:   src/my_dynamixel_tutorial/scripts/state_publisher.py

--- src/my_dynamixel_tutorial/scripts/state_publisher.py
+++ src/my_dynamixel_tutorial/scripts/state_publisher.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import JointState as sensor_msg_joint_state
+from sensor_msgs.msg import JointState
 from dynamixel_msgs.msg import MotorState
 from dynamixel_msgs.msg import MotorStateList
-from dynamixel_msgs.msg import JointState as dynamixel_joint_state
 from math import pi
 import time
 
@@ -12,13 +11,15 @@ rospy.init_node('State_Publisher')
 
 # Name of the joints in the robotic arm
 # Name of motor ID [1,2,3,4,5,6] is specified in the list.
-joints = ["joint_5f"]
+joints = ["shoulder_yaw", "shoulder_pitch", "elbow", "wrist_pitch", "wrist_roll"]
 
 #Dynamixel Motors will not be in Position 0 when switched ON. Dynamixel motors will have some initial position value. 
 #In the URDF it is assumed that initial joint value is 0 radian. 
 #Hence offset is required to match the URDF and the real robot.
 #Offset for motor id [1,2,3,4,5,6] is specified in the list.
-offset = [202]
+# offset = [666,512,512,375,512,682]
+# offset = [185, 3000, 2222, 512, 202]
+offset = [1989, 3043, 2220, 509, 620]
 
 '''
 Function: process(). Callback for subscriber of raw data from dynamixel motor. 
@@ -27,25 +28,24 @@ Logic: Dynamixel position = 0 for 0 degree and Dynamixel position = 1023 for 300
        Where position = feddback-offset.
 '''
 def process(msg):
-	sensor_messages = sensor_msg()
-	joint_states.header.stamp = rospy.Time.now()
-    joint_states.position.append()
-    x = msg.current_pos
-    joi
-	# for x in msg.motor_states:
-	# 	joint_states.name.append(joints[x.id-1])
+	joint_states = JointState()
 
-		# if(not (x.id==2 or x.id==4)):
-		# 	joint_states.position.append((x.position-offset[x.id-1])*(300.0/1023)*(pi/180))
-		# else:
-		# 	joint_states.position.append((offset[x.id-1]-x.position)*(300.0/1023)*(pi/180))
-		# #joint_states.velocity.append(x.velocity)
-		
+	joint_states.header.stamp = rospy.Time.now()
+
+	for x in msg.motor_states:
+		joint_states.name.append(joints[x.id-1])
+
+		if((x.id < 4)):
+			joint_states.position.append((x.position-offset[x.id-1])*(360.0/4096)*(pi/180))
+		else:
+			joint_states.position.append((offset[x.id-1]-x.position)*(300.0/1023)*(pi/180))
+		#joint_states.velocity.append(x.velocity)
+	
 	pub.publish(joint_states)
 
 # Subscriber for raw feedback from dynamixel motor. Position of the motor will be in the range of (0,1023).
-sub = rospy.Subscriber('/joint5_controller/states',JointState,process)
+sub = rospy.Subscriber('/motor_states/pan_tilt_port',MotorStateList,process)
 # Publisher for the current position of dynamixel motor in radian
-pub = rospy.Publisher('/robotic_arm/joint_states',sensor_msg,queue_size=10)
+pub = rospy.Publisher('/robot/joint_states',JointState,queue_size=10)
 
 rospy.spin()
