836e7b7fed681e0d472ee66f56cb58db8286ed0e
Viraj Sonawane <majorviraj@gmail.com>
2019-02-01 14:51:11 +0530

    Final Urdf and xacro done

---
+++ src/my_dynamixel_tutorial/scripts/gripper_controller.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+from dynamixel_msgs.msg import JointState
+
+# rate = rospy.Rate(10) # 10hz
+# rate.sleep()
+
+load_threshold = 0.1
+
+def open_gripper():
+    # while not rospy.is_shutdown():
+    gripper_pub.publish(1) # 1 radians
+
+def state_callback(current_state):
+    global current_goal_pos
+    global current_load
+    current_load = current_state.load
+    current_goal_pos = current_state.goal_pos 
+    if (current_load < 0):
+        current_load = current_load * (-1)
+    
+    # print "current_goal_pos", current_goal_pos
+
+def close_gripper():
+    print "in close_gripper"
+    global current_goal_pos
+    global current_load
+
+    angle = current_goal_pos
+    
+    while(current_load < load_threshold) :
+        print "angle ", angle,
+        print " current_load ", current_load
+        angle = angle - 0.05
+        # if (angle<0):
+        #     angle = 0
+        gripper_pub.publish(angle)
+    print "close_gripper END"
+    print "angle ", angle,
+    print " current_load ", current_load
+
+if __name__ == '__main__':
+    try:
+        gripper_pub = rospy.Publisher('/gripper_controller/command', Float64, queue_size=10)
+        rospy.Subscriber('/gripper_controller/state', JointState, state_callback)
+        rospy.init_node('gripper_controller', anonymous=True)
+        # open_gripper()
+        # rospy.sleep(3.)
+        close_gripper()
+        rospy.spin()
+
+        exit()
+    
+    except rospy.ROSInterruptException:
+        raise e
+        print "Exeption"
+        exit()
+
